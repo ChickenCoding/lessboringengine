605627d56d78609122e8dbc357c7c61413fb2de3
David Colgan <dvcolgan@gmail.com>
2014-03-08 15:24:49 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            dist: {
+                src: ['src/**/*.js', 'lib/**/*.js', 'vendor/**/*.js'],
+                dest: 'build/app.js'
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
+            options: {
+                debug: true
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['**/*.js'],
+                tasks: ['jshint', 'browserify'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint', 'browserify']);
+};
